56f319d5ba8bdea0b1ef2ca736ad8dfdedc9f54a
ashandileo <ashandileonadi2001@gmail.com>
2020-10-04 17:29:58 +0700

    remove warning

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,8 @@
-import React, { Children } from 'react';
+import React from 'react';
 
 const Card = ({ children, className }) => {
   return (
-    <div className={`${className} tw-border tw-py-4 tw-px-4 tw-flex tw-flex-col tw-items-center tw-bg-transparent hover:tw-bg-gray-300 tw-rounded`}>
+    <div className={`${className || ''} tw-border tw-py-4 tw-px-4 tw-flex tw-flex-col tw-items-center tw-bg-transparent hover:tw-bg-gray-300 tw-rounded`}>
       {children}
     </div>
   )
